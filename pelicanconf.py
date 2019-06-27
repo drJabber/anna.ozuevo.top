46be637f80d8c17a23e4fe596386210cf917474b
drJabber@gmail.com <dJabber@gmail.com>
2019-06-27 13:17:14 +0300

    changed some settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,11 +4,11 @@ from __future__ import unicode_literals
 
 AUTHOR = 'dJabber'
 SITENAME = 'AnnaBake'
-SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Moscow'
+SITEURL='https://anna.ozuevo.top'
 
 DEFAULT_LANG = 'ru'
 
@@ -20,10 +20,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -31,5 +28,6 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+LOAD_CONTENT_CACHE=False
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
